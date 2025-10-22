89b0366a9b4da847391c3c91c8c83719b86a45eb
Duskitten <theduskitty@gmail.com>
2025-10-22 03:09:30 +0000

    add math values

--- revel-define.h
+++ revel-define.h
@@ -20,6 +20,23 @@ extern "C"{
 #include <string.h>
 #include <malloc.h>
 
+// Found here: https://stackoverflow.com/questions/4353525/floating-point-linear-interpolation
+float lerp(float a, float b, float f)
+{
+    return a * (1.0 - f) + (b * f);
+}
+
+// Found here: https://stackoverflow.com/questions/427477/fastest-way-to-clamp-a-real-fixed-floating-point-value
+float clamp_float(float value, float min_val, float max_val) {
+    if (value < min_val) {
+        return min_val;
+    } else if (value > max_val) {
+        return max_val;
+    } else {
+        return value;
+    }
+}
+
 #if __cplusplus__
 };
 #endif
